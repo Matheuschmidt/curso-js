449133098aaa3fbc23a1c84607a70dacc5bc347c
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-07 20:15:07 -0300

    pt1 ex007

---
+++ aula06/ex017.js
@@ -0,0 +1,25 @@
+var n1 = 2;
+var n2 = 3;
+
+
+ function soma() {
+    return n1 + n2;
+ }
+
+ function subtracao(){
+    return n1 - n2;
+ }
+
+ function multiplicacao(){
+    return n1 * n2;
+ }
+
+ function divisao(){
+    if(n2!= 0){
+        return n1 / n2;
+    }else{
+        return "Não é possivel dividir por zero";
+    }
+
+    
+ }
