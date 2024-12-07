9b5ad7b3d184432faab5c00194ca9317fc705e05
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-06 21:02:50 -0300

    part1 condicoes

---
+++ aula05/ex012.js
@@ -0,0 +1,12 @@
+var vel = 100.1;
+
+console.log(`a velocidade q voce esta é ${vel}km/h`);
+
+if (vel > 100){ // Condição Composta
+    console.log('Voce esta acima da velocidade permitida!');
+}else{
+    console.log('Voce esta dentro da velocidade permitida.');
+}
+
+
+console.log('Dirija com seguranca. Use cinto de seguranca');
