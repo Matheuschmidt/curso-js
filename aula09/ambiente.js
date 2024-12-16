c097035d5d9fcdfd1c089c0ade581f3ba192591e
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-16 20:53:48 -0300

    repeticao com array

--- aula09/ambiente.js
+++ aula09/ambiente.js
@@ -1,3 +1,10 @@
-let num =[4, 8, 0 ,3];
+var num =[4, 8, 10 ,3, 2, 5.6, 1, 1.5, 5, 7, 15, 9];
 
-console.log(`Nosso array eh o ${num}`);
+num.sort();
+console.log(`${num}`);
+console.log(`O array tem  ${num.length} posiçoes`);
+//console.log(`${num[11]}`);
+
+for (var pos = 0; pos < num.length; pos++){
+    console.log(`O elemento da posiçao ${pos} é ${num[pos]}`);
+}
