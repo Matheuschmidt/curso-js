374fb60908c1af0c841121781b17dd1849c25402
matheuschmidt <matheuschmidt.dev@gmail.com>
2024-12-16 21:15:19 -0300

    indexOf

---
+++ aula09/indexOf.js
@@ -0,0 +1,9 @@
+var num = [1, 2, 3, 4];
+
+let pos = num.indexOf(4);
+
+if(pos == -1) {
+    console.log("O número não está presente no array");
+} else {
+    console.log(`O número está na posição ${pos}`);
+}
